b8ceed1dea27493aff72dc53f99b2dd7dab7bba6
Craigson <craigpick@gmail.com>
2015-09-19 20:15:36 -0400

    basic site up and running

--- week1/script.js
+++ week1/script.js
@@ -5,7 +5,6 @@ function changeImage(img) {
 
 	if (currentSource === "images/week1_1.jpg"){
 		document.getElementById("img").src = img.src.replace("images/week1_1.jpg", "images/week1_2.jpg");
-		
 	}
 	console.log(currentSource);
 };
