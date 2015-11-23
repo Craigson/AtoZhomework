735ee6d31edca7d6212f1c7a9e8a995b1eb6ba77
Craigson <craigpick@gmail.com>
2015-11-23 12:12:01 -0500

    adding final project proposal

---
+++ final_proposal/script.js
@@ -0,0 +1,11 @@
+
+
+function changeImage(img) {
+	var currentSource = document.getElementById("img").src;
+
+	if (currentSource === "images/week1_1.jpg"){
+		document.getElementById("img").src = img.src.replace("images/week1_1.jpg", "images/week1_2.jpg");
+	}
+	console.log(currentSource);
+};
+
